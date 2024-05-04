9e1db765941fee5f7a1cf73e39a30c7c8e7833ed
Convenience  Chibatamoto <conveniencechibatamoto@gmail.com>
2024-05-04 20:43:30 +0200

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -1,5 +1,10 @@
 import smtplib, ssl
 import os
+import streamlit as st
+from send_email import send_email
+import pandas
+
+df = pandas.read_csv("topics.csv")
 
 def send_email(message):
     host = "smtp.gmail.com"
@@ -11,6 +16,21 @@ def send_email(message):
     receiver = "conveniencechibatamoto@gmail.com"
     context = ssl.create_default_context()
 
-    with smtplib.SMTP_SSL(host, port, context=context) as server:
-        server.login(username, password)
-        server.sendmail(username, receiver, message)
+   with st.form(key="email_form"):
+    user_email = st.text_input("Your Email Address")
+    option = st.selectbox(
+        'What topic do you want to discuss?',
+        df["topic"]
+    )
+    raw_message = st.text_area("Text")
+    message = f"""
+Subject: New email from {user_email}
+From: {user_email}
+Topic: {option}
+{raw_message}
+"""
+    button = st.form_submit_button("Submit")
+
+if button:
+    send_email(message)
+    st.info("Your email was sent successfully!")
